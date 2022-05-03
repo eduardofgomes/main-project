4b02eeba4967b93f6858ae9eb0e45534b5fa5807
eduardofgomes <eduardofranciscogomes01@gmail.com>
2022-05-03 11:46:53 -0300

    03.05

---
+++ src/conexao/conn.php
@@ -0,0 +1,12 @@
+<?php
+
+$hostname = "sql102.epizy.com";
+$database = "epiz_31454928_XXX";
+$username = "epiz_31454928";
+$password = "GEA1TmPNzz";
+
+if($conecta = mysqli_connect($hostname, $username, $password, $database)){
+    echo 'Conectado ao banco de dados '.$database.'.....';
+} else {
+    echo 'Erro: '.mysqli_connect_error();
+}
