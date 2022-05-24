35a78c109e8afa566a93d3a6ad8e3b3eeb79a31b
eduardofgomes <eduardofranciscogomes01@gmail.com>
2022-05-23 21:26:19 -0300

    23.05

--- src/tipo/controle/new-tipo.js
+++ src/tipo/controle/new-tipo.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
         $('.modal-title').empty()
         $('.modal-body').empty()
 
-        $('.modal-title').apppend('Adicionar novo tipo de acesso')
+        $('.modal-title').append('Adicionar novo tipo de acesso')
         
         $('.modal-body').load('src/tipo/visao/form-tipo.html')
 
