a3e060cbf1262d93ffa243ad746373d33826a9e9
eduardofgomes <eduardofranciscogomes01@gmail.com>
2022-08-16 12:17:11 -0300

    16.08

--- src/vendedor/modelo/login-vendedor.php
+++ src/vendedor/modelo/login-vendedor.php
@@ -2,13 +2,17 @@
 
 include('../../conexao/conn.php');
 
-$sql =$pdo->query("SELECT *, count(ID) as achou FROM  VENDEDOR WHERE LOGIN  = '".$_REQUEST['LOGIN']."' AND SENHA = '".md5($_REQUEST['SENHA']."'"); 
+$sql =$pdo->query("SELECT *, count(ID) as achou FROM  VENDEDOR WHERE LOGIN  ='".$_REQUEST['LOGIN']."'AND SENHA ='".md5($_REQUEST['SENHA'])."'"); 
 
 while($resultado = $sql->fetch(PDO::FETCH_ASSOC)) {
     if($resultado['achou'] == 1) {
         session_start();
         $_SESSION['NOME'] = $resultado['NOME'];
         $_SESSION['TIPO'] = $resultado['TIPO_ID'];
+        $dados = array(
+            'tipo' => 'success',
+            'mensagem' => 'Você entrou'
+        );
         
     } else {
         $dados = array(
