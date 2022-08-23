639eb085075fd9ece5fe56a756b678c4f5620d5d
eduardofgomes <eduardofranciscogomes01@gmail.com>
2022-08-23 11:47:18 -0300

    23.08

---
+++ src/vendedor/controle/logout-vendedor.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+    $('#logout').click(function(e){
+        e.preventDefault();
+
+    
+
+    $.ajax({
+        type: 'POST',
+        dataType: 'json',
+        assync: true,
+        url: 'src/vendedor/modelo/logout-vendedor.php',
+        success: function(dados) {
+            if(dados.tipo == 'success') {
+                $(location).attr('href', 'index.html')
+            } else {
+                Swal.fire({
+                    title: 'e-rifa',
+                    text: dados.mensagem,
+                    icon: dados.tipo,
+                    confirmButtonText: 'OK'
+                })
+            }
+        }
+    })
+    })
+})
