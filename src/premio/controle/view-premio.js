4d5cb9a7645f90ce8718b84a478617e0ed203070
eduardofgomes <eduardofranciscogomes01@gmail.com>
2022-09-12 18:52:14 -0300

    12.09

--- src/premio/controle/view-premio.js
+++ src/premio/controle/view-premio.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
 
-    $('#table-premio').on('click', 'button.btn-edit', function(e) {
+    $('#table-premio').on('click', 'button.btn-view', function(e) {
 
-        e.preventDefault()
+        e.preventDefault();
 
         // Alterar as informações do modal para apresentação dos dados
 
@@ -23,14 +23,17 @@ $(document).ready(function() {
                 if (dado.tipo == "success") {
                     $('.modal-body').load('src/premio/visao/form-premio.html', function() {
                         $('#NOME').val(dado.dados.NOME)
+                        $('#NOME').attr('readonly', 'true')
                         $('#DESCRICAO').val(dado.dados.DESCRICAO)
+                        $('#DESCRICAO').attr('readonly', 'true')
                         $('#VALOR').val(dado.dados.VALOR)
+                        $('#VALOR').attr('readonly', 'true')
                         $('#PROMOCAO_ID').empty()
                         $('#ID').val(dado.dados.ID)
 
                         var PROMOCAO_ID = dado.dados.PROMOCAO_ID
 
-                        //Consultar todos os tipos cadastrados no banco de dados
+                        //Consultar todos os tipos cadastrados no banco de daods
                         $.ajax({
                             dataType: 'json',
                             type: 'POST',
@@ -39,22 +42,21 @@ $(document).ready(function() {
                             success: function(dados) {
                                 for (const result of dados) {
                                     if (result.ID == PROMOCAO_ID) {
-                                        $('#PROMOCAO_ID').append(`<option value="${result.ID}" selected>${result.TITULO}</option>`)
-                                    } else {
                                         $('#PROMOCAO_ID').append(`<option value="${result.ID}">${result.TITULO}</option>`)
                                     }
+
                                 }
                             }
                         })
+
                     })
-                    $('.btn-save').show()
-                    $('.btn-save').removeAttr('data-operation')
+                    $('.btn-save').hide()
                     $('#modal-premio').modal('show')
                 } else {
                     Swal.fire({ // Inicialização do SweetAlert
-                        title: 'E-RIFA', // Título da janela SweetAlert
+                        title: 'e-Rifa', // Título da janela SweetAler
                         text: dado.mensagem, // Mensagem retornada do microserviço
-                        type: dado.tipo, // premio de retorno [success, info ou error]
+                        type: dado.tipo, // vendedor de retorno [success, info ou error]
                         confirmButtonText: 'OK'
                     })
                 }
